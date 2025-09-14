b0c17d55c6aab7a13fddb101ca305ed688410c1f
binary-ho <jinho.lee16@cj.net>
2025-09-14 13:58:33 +0900

    Add test file for diff analysis

---
+++ test-diff-analysis.js
@@ -0,0 +1,8 @@
+// 테스트용 파일 - diff 분석을 위한 간단한 변경사항 생성
+console.log('Modified line 1');
+console.log('Original line 2');
+console.log('New added line');
+console.log('Original line 3');
+console.log('Another modified line 4');
+console.log('Original line 5');
+console.log('Final added line');
